cb503ef3bcc3301d406f738a102d353bd96f1f0c
Louis <Louis.Medina@flatironschool.com>
2022-10-31 11:58:00 -0500

    add starter code for components and props lecture

---
+++ 01_components_and_props/project_showcase/src/components/ProjectList.js
@@ -0,0 +1,9 @@
+// - Accept the props provided to the component
+
+// - Destructure `projects` from the props object
+
+// - Map over the `projects` array to render the `ProjectListItem` component for each element:
+
+//   - Provide a key prop set to the `project id` value
+
+//   - Provide each `project` object as a prop named `project`
