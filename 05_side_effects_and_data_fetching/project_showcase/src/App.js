f3ef3b3c49f0ee91649656ff0fe4e86e9bbadbbd
Louis <Louis.Medina@flatironschool.com>
2022-11-07 16:35:25 -0500

    add p2 notes for 05 lecture

--- 05_side_effects_and_data_fetching/project_showcase/src/App.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/App.js
@@ -7,7 +7,7 @@ import Timer from "./components/Timer";
 const App = () => {
   const [projects, setProjects] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
-  const [isVisible, setIsVisible] = useState(true);
+  const [isTimerVisible, setIsTimerVisible] = useState(true);
 
   // const handleClick = () => {
   //   fetch("http://localhost:4000/projects")
@@ -15,22 +15,42 @@ const App = () => {
   //     .then((projects) => setProjects(projects));
   // };
 
+  // useEffect Always Accepts Two Arguments
+    // 1st => Callback Function (Effect)
+    // 2nd => Dependency Array (Optional / Constrains Our useEffect Behaviors)
+
+  // Outside of useEffect - No Way to Constrain
+  // fetch("http://localhost:4000/projects")
+  //   .then((res) => res.json())
+  //   .then((projects) => setProjects(projects));
+
+  // console.log("App Component Rendered");
+
   // Implement useEffect Hook
   useEffect(() => {
+    
+    // console.log("test");
+
+    // Not Necessary to Clean Up Behavior
     fetch("http://localhost:4000/projects")
       .then((res) => res.json())
       .then((projects) => setProjects(projects));
+
+    // Clean Up Function (Optional) - No Need Here
+    // return () => {
+
+    // }
   }, []);
 
   // Function to Mount / UnMount Our Timer Component
-  const toggleTimer = () => setIsVisible(!isVisible);
+  const toggleTimer = () => setIsTimerVisible(!isTimerVisible);
 
   const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
 
   return (
     <div className={isDarkMode ? "App" : "App light"}>
-      { isVisible ? <Timer /> : null }
-      <button onClick={toggleTimer}>{ isVisible ? "Hide Timer" : "Show Timer"}</button>
+      { isTimerVisible ? <Timer /> : null }
+      <button onClick={toggleTimer}>{ isTimerVisible ? "Hide Timer" : "Show Timer"}</button>
       <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
       <ProjectForm />
       {/* <button onClick={handleClick}>Load Projects</button> */}
--- 05_side_effects_and_data_fetching/project_showcase/src/components/Timer.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/components/Timer.js
@@ -1,32 +1,58 @@
 import { useState, useEffect } from "react";
 
 const Timer = () => {
+    
+    // console.log("Timer Component Rendered");
+
+    // useEffect(() => {
+        
+    //     // Behavior First
+
+    //     // Return / Clean Up Later
+    //     return (
+    //         Clean Up Behavior
+    //     );
+    // });
+
     // Some State to Manage Number of Seconds
     const [ count, setCount ] = useState(0);
+
+    // console.log("Component Mounted!");
+    // console.log(count);
     
+    const consoleTest = () => console.log("Page Clicked!");
+
+    // console.log(count);
+
     useEffect(() => {
+
+        // Add Event Listener to Fire Off When User Clicks On Page
+        document.addEventListener("click", consoleTest);
+
+        // document.addEventListener("click", someOtherFunction);
         
-        let count = setTimeout(() => {
-            // console.log("Side Effect Fired Off!");
-            
+        let intervalId = setInterval(() => {
             setCount(prevCount => prevCount + 1);
         }, 1000);
 
-        // Clean Up Function Fires Off At Two Times:
-            // When The Component Initially Mounts
-            // When The Component Unmounts
-
-        // counst => intervalID Necessary for Clearing Out setTimeout
-        // console.log(count);
+        // intervalId => intervalID Necessary for Clearing Out setTimeout
+        // console.log(intervalId);
 
         // clearTimeout => Built In JS Function to Clear Out Previous setTimeout Intervals
-        return () => clearTimeout(count);
-    });
+        return () => { 
+            
+            // Clean Up Function Fires Off At Two Times:
+                // When The Component Initially Mounts
+                // When The Component Unmounts
+            // console.log("Clean Up Fired Off!");
+            
+            // Clear Out setTimeout
+            clearInterval(intervalId); 
 
-    // Add Function to Clear Out setTimeout
-    // const stopTimer = () => {
-    //     clearTimeout(count);
-    // }
+            // Remove Event Listener
+            document.removeEventListener("click", consoleTest);
+        };
+    }, []);
 
     return (
         <>
